a96936175114b8f171504bf3a98fbd4434c43aa5
PraisesPJMT <praisesmusa@outlook.com>
2025-04-13 11:58:00 +0100

    `feat:` add theme-toggle button component

---
+++ components/theme-toggle.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+
+export function ThemeToggle() {
+  const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Fix hydration issue
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // Avoids hydration mismatch on SSR
+  }
+
+  return (
+    <button
+      type="button"
+      className="fixed top-4 text-primary right-4 p-2 border border-primary rounded-full bg-secondary cursor-pointer"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      title={resolvedTheme === "dark" ? "Light Mode" : "Dark Mode"}
+      aria-label={resolvedTheme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      aria-describedby="theme-toggle"
+    >
+      {resolvedTheme === "dark" ? <MdLightMode /> : <MdDarkMode />}
+    </button>
+  );
+}
