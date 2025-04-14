d26b486a9347d02e08538025f9f60a9aa441cf73
PraisesPJMT <praisesmusa@outlook.com>
2025-04-14 14:19:04 +0100

    `chore:` add template Projects component

---
+++ app/projects/page.js
@@ -0,0 +1,18 @@
+import { Section, WrapSection } from "@/components/sections";
+
+export const metadata = {
+  title: "Praises Tula Projects | Software Engineer & Web Developer",
+  description: "Discover Praises Tula's projects, showcasing his expertise in software engineering and web development.",
+};
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <Section>
+        <WrapSection>
+          <h1>Praises Tula - Projects</h1>
+        </WrapSection>
+      </Section>
+    </>
+  );
+}
