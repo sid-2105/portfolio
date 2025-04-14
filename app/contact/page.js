aeea63a6e2316bcd08c9587e5e1a3bcd69cce211
PraisesPJMT <praisesmusa@outlook.com>
2025-04-14 14:18:53 +0100

    `chore:` add template Contact component

---
+++ app/contact/page.js
@@ -0,0 +1,18 @@
+import { Section, WrapSection } from "@/components/sections";
+
+export const metadata = {
+  title: "Contact Praises Tula | Software Engineer & Web Developer",
+  description: "Reach out to Praises Tula for inquiries, collaborations, or just to say hello!",
+};
+
+export default function ContactPage() {
+  return (
+    <>
+      <Section>
+        <WrapSection>
+          <h1>Praises Tula - Contact</h1>
+        </WrapSection>
+      </Section>
+    </>
+  );
+}
