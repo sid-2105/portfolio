e8f00ac18eaf26c952f1d92f86769b32a19a88c6
PraisesPJMT <praisesmusa@outlook.com>
2025-04-16 17:52:38 +0100

    feat: add text components

---
+++ components/texts.js
@@ -0,0 +1,23 @@
+export const PurpleText = ({ children, className }) => {
+  return (
+    <span className={`text-purple ${className}`}>{children}</span>
+  );
+};
+
+export const PrimaryText = ({ children, className }) => {
+  return (
+    <p className={`text-primary ${className}`}>{children}</p>
+  );
+};
+
+export const SecondaryText = ({ children, className }) => {
+  return (
+    <p className={`text-secondary ${className}`}>{children}</p>
+  );
+};
+
+export const TetiaryText = ({ children, className }) => {
+  return (
+    <p className={`text-tertiary ${className}`}>{children}</p>
+  );
+};
