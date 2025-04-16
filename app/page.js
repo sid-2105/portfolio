1d639e922e4bb9d1d9cf043d4655a10a208d2115
PraisesPJMT <praisesmusa@outlook.com>
2025-04-16 17:56:04 +0100

    feat: add hello section to home page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-import Header from "@/components/header";
 import { Section, WrapSection } from "@/components/sections";
+
+import Header from "@/components/header";
+import Hello from "@/components/home/hello";
 
 export default function Home() {
   return (
     <>
-      <Section>
-        <WrapSection>
-          <h1>Praises Tula</h1>
-        </WrapSection>
-      </Section>
+      <Hello />
     </>
   );
 }
