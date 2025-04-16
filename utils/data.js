f339c95876b28f75da39ac60f7c649121cd72ae1
PraisesPJMT <praisesmusa@outlook.com>
2025-04-16 17:52:55 +0100

    feat: add app data

---
+++ utils/data.js
@@ -0,0 +1,66 @@
+export const TESTIMONIALS = [
+  {
+    name: "Michael Mesfin",
+    profile_picture: "/images/mishael_mesfin.png",
+    location: "Addis Ababa, Ethiopia",
+    quote: 'Praises is a great collaborator. When working with him\n'
+      + ' he makes you feel calm and interested. He has proven\n'
+      + ' himself as reliable, exceptionally having great attention to\n'
+      + ' detail and his obsession with making great UI transpires through\n'
+      + ' his work.\n'
+      + ' I would recommend\n'
+      + ' Praises for the role of Software Developer and so should you!',
+    link: "https://www.linkedin.com/in/michael-mesfin-haileyesus/"
+  },
+  {
+    name: "Sharon Kessio",
+    profile_picture: "/images/sharon_kessio.png",
+    location: "Nairobi, Kenya",
+    quote: 'Praise is easy to '
+      + 'work with as he keeps time, fulfilled his part '
+      + 'of agreements on group projects, well versed with the topics '
+      + 'at hand as he did prior research, he is also keen '
+      + 'on details which reduced time spent fixing bugs. I '
+      + 'enjoyed working with Praises.',
+    link: "https://www.linkedin.com/in/sharon-kessio/"
+  },
+  {
+    name: "Aakash Verma",
+    profile_picture: "/images/aakash_verma.png",
+    location: "Chennai, India",
+    quote: 'Praises is an intelligent and deeply creative person. '
+      + 'He is thorough and has a great eye for detail. When Praises '
+      + 'was working on a project, our error-correction needs were always '
+      + 'reduced by half or more thanks to his outstanding diligence and '
+      + 'attention to detail. In short, Praises is a delight and an incredible '
+      + 'asset to anyone lucky enough to work with him.',
+    link: "https://www.linkedin.com/in/skyv2022/"
+  },  
+];
+
+export const QUOTES = [
+  {
+    quote: "Code is like humor. When you have to explain it, it's bad.",
+    author: "Cory House"
+  },
+  {
+    quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    author: "Martin Fowler"
+  },
+  {
+    quote: "First, solve the problem. Then, write the code.",
+    author: "John Johnson"
+  },
+  {
+    quote: "The best error message is the one that never shows up.",
+    author: "Thomas Fuchs"
+  },
+  {
+    quote: "Optimism is an occupational hazard of programming: feedback is the treatment.",
+    author: "Kent Beck"
+  },
+  {
+    quote: "Software and cathedrals are much the same — first we build them, then we pray.",
+    author: "Paul Graham"
+  },  
+];
